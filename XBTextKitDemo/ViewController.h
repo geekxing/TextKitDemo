f5a87c063413e189032c9770845443c797783ec1
geekxing <842775263@qq.com>
2019-04-17 23:03:26 +0800

    Initial Commit

---
+++ XBTextKitDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  XBTextKitDemo
+//
+//  Created by 赖霄冰 on 2019/4/17.
+//  Copyright © 2019 赖霄冰. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
